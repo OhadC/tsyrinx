e48e206c3a192768f655064141768095d3d7bdd8
Ohad Cohen <ohadc.me@gmail.com>
2025-11-09 22:14:20 +0200

    fix export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export * from "./decorators";
 export { instance as container } from "./dependency-container";
 export * from "./factories";
 // export * from './injectors';
+export * from "./injectors";
 export { delay } from "./lazy-helpers";
 export * from "./providers";
 export { Lifecycle, type DependencyContainer, type Disposable, type Frequency, type RegistrationOptions } from "./types";
